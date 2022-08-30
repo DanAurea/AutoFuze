760e9c67530cf91fdd1d1cc3a1b37213e82742c6
DanAurea <laochi.bc@gmail.com>
2022-08-30 21:42:44 +0200

    Add message type for SOME/IP TP
    SOME/IP TP should be feasible since adding of message types related to transport of larger payload.

--- protocols/some_ip/enum/message_type.py
+++ protocols/some_ip/enum/message_type.py
@@ -4,6 +4,11 @@ class MessageType(IntEnum):
     REQUEST               = 0X00 # A request expecting a response (even void)
     REQUEST_NO_RETURN     = 0X01 # A fire & forget request
     NOTIFICATION          = 0X02 # A request of a notification expecting no response
+    TP_REQUEST            = 0X20 # TP request
+    TP_REQUEST_NO_RETURN  = 0X21 # TP fire & forget request
+    TP_NOTIFICATION       = 0X22 # TP request of a notification/event call
+    TP_RESPONSE           = 0X23 # TP response
+    TP_ERROR              = 0X24 # TP response containing error
     REQUEST_ACK           = 0X40 # ACK for request (optional)
     REQUEST_NO_RETURN_ACK = 0X41 # ACK for request no return (informational)
     NOTIFICATION_ACK      = 0X42 # ACK for notification (informational)
